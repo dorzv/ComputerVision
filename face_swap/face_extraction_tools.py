4f31e9bba561aa5153382b5f94fd78c4e6203257
dor <dorzvu2@gmail.com>
2023-04-23 20:51:36 +0300

    change the method for alignment from using opencv recognition module to manual affine transform

--- face_swap/face_extraction_tools.py
+++ face_swap/face_extraction_tools.py
@@ -2,9 +2,10 @@ import cv2
 from pathlib import Path
 from shutil import rmtree
 from urllib.request import urlretrieve
+import numpy as np
 from tqdm import tqdm
 
-def extract_frames_from_video(video_path: str, output_folder: str, frames_to_skip: int=0):
+def extract_frames_from_video(video_path: str, output_folder: str, frames_to_skip: int=0) -> None:
     """
     Extract frame from video as a JPG images.
     Args:
@@ -44,11 +45,12 @@ def extract_frames_from_video(video_path: str, output_folder: str, frames_to_ski
     print(f'{saved_frame_counter} of {extract_frame_counter} frames saved')
 
 
-def extract_and_align_face_from_image(input_dir: str):
+def extract_and_align_face_from_image(input_dir: str, desired_face_width: int=256) -> None:
     """
     Extract the face from an image, align it and save to a directory inside in the input directory
     Args:
         input_dir (str): path to the directory contains the images extracted from a video
+        desired_face_width (int): the width of the aligned imaged in pixels
 
     Returns:
 
@@ -59,6 +61,8 @@ def extract_and_align_face_from_image(input_dir: str):
     if output_dir.exists():
         rmtree(output_dir)
     output_dir.mkdir()
+
+    desired_face_height = desired_face_width
 
     image = cv2.imread(str(input_dir / '00000.jpg'))
     image_height = image.shape[0]
@@ -72,15 +76,10 @@ def extract_and_align_face_from_image(input_dir: str):
         filename, headers = urlretrieve(url, filename=str(detection_model_path))
         print('Download finish!')
 
-    recognition_model_path = Path('models/face_recognition_sface_2021dec.onnx')
-    if not recognition_model_path.exists():
-        url = "https://github.com/opencv/opencv_zoo/raw/master/models/face_recognition_sface/face_recognition_sface_2021dec.onnx"
-        print('Downloading face recognition model...')
-        filename, headers = urlretrieve(url, filename=str(recognition_model_path))
-        print('Download finish!')
-
     detector = cv2.FaceDetectorYN.create(str(detection_model_path), "", (image_width, image_height))
-    recognizer = cv2.FaceRecognizerSF.create(str(recognition_model_path), "")
+
+    left_eye_desired_coordinate = np.array((0.37, 0.37))
+    right_eye_desired_coordinate = np.array((1 - left_eye_desired_coordinate[0], left_eye_desired_coordinate[1]))
 
     for image_path in tqdm(list(input_dir.glob('*.jpg'))):
         image = cv2.imread(str(image_path))
@@ -88,5 +87,28 @@ def extract_and_align_face_from_image(input_dir: str):
         ret, faces = detector.detect(image)
         if faces is None:
             continue
-        face_aligned = recognizer.alignCrop(image, faces[0])
+
+        # align the face
+        # get coordinate of the center of the eyes in the image
+        right_eye = faces[0, 4:6]
+        left_eye = faces[0, 6:8]
+
+        # compute the angle of the right eye relative to the left eye
+        dist_eyes_x = right_eye[0] - left_eye[0]
+        dist_eyes_y = right_eye[1] - left_eye[1]
+        dist_between_eyes = np.sqrt(dist_eyes_x ** 2 + dist_eyes_y ** 2)
+        angles_between_eyes = np.rad2deg(np.arctan2(dist_eyes_y, dist_eyes_x) - np.pi)
+        eyes_center = (left_eye + right_eye) // 2
+
+        desired_dist_between_eyes = desired_face_width * (right_eye_desired_coordinate[0] - left_eye_desired_coordinate[0])
+        scale = desired_dist_between_eyes / dist_between_eyes
+
+        M = cv2.getRotationMatrix2D(eyes_center, angles_between_eyes, scale)
+
+        M[0, 2] += 0.5 * desired_face_width - eyes_center[0]
+        M[1, 2] += left_eye_desired_coordinate[1] * desired_face_height - eyes_center[1]
+
+        face_aligned = cv2.warpAffine(image, M, (desired_face_width, desired_face_height), flags=cv2.INTER_CUBIC)
+
         cv2.imwrite(str(output_dir / f'{image_path.name}'), face_aligned, [cv2.IMWRITE_JPEG_QUALITY, 90])
+
