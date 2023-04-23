424c9b634514ad08ed629a874dd8dd38490cd520
dor <dorzvu2@gmail.com>
2023-04-23 12:50:05 +0300

    add function to extract and align faces from images

--- face_swap/face_extraction_tools.py
+++ face_swap/face_extraction_tools.py
@@ -1,5 +1,8 @@
 import cv2
 from pathlib import Path
+from shutil import rmtree
+from urllib.request import urlretrieve
+from tqdm import tqdm
 
 def extract_frames_from_video(video_path: str, output_folder: str, frames_to_skip: int=0):
     """
@@ -33,9 +36,57 @@ def extract_frames_from_video(video_path: str, output_folder: str, frames_to_ski
             break
 
         if extract_frame_counter % (frames_to_skip + 1) == 0:
-            cv2.imwrite(str(output_folder / f'{saved_frame_counter:05d}.jpg'), frame, [cv2.IMWRITE_JPEG_QUALITY, 100])
+            cv2.imwrite(str(output_folder / f'{saved_frame_counter:05d}.jpg'), frame, [cv2.IMWRITE_JPEG_QUALITY, 90])
             saved_frame_counter += 1
 
         extract_frame_counter += 1
 
     print(f'{saved_frame_counter} of {extract_frame_counter} frames saved')
+
+
+def extract_and_align_face_from_image(input_dir: str):
+    """
+    Extract the face from an image, align it and save to a directory inside in the input directory
+    Args:
+        input_dir (str): path to the directory contains the images extracted from a video
+
+    Returns:
+
+    """
+
+    input_dir = Path(input_dir)
+    output_dir = input_dir / 'aligned'
+    if output_dir.exists():
+        rmtree(output_dir)
+    output_dir.mkdir()
+
+    image = cv2.imread(str(input_dir / '00000.jpg'))
+    image_height = image.shape[0]
+    image_width = image.shape[1]
+
+    detection_model_path = Path('models/face_detection_yunet_2022mar.onnx')
+    if not detection_model_path.exists():
+        detection_model_path.parent.mkdir(parents=True, exist_ok=True)
+        url = "https://github.com/opencv/opencv_zoo/raw/master/models/face_detection_yunet/face_detection_yunet_2022mar.onnx"
+        print('Downloading face detection model...')
+        filename, headers = urlretrieve(url, filename=str(detection_model_path))
+        print('Download finish!')
+
+    recognition_model_path = Path('models/face_recognition_sface_2021dec.onnx')
+    if not recognition_model_path.exists():
+        url = "https://github.com/opencv/opencv_zoo/raw/master/models/face_recognition_sface/face_recognition_sface_2021dec.onnx"
+        print('Downloading face recognition model...')
+        filename, headers = urlretrieve(url, filename=str(recognition_model_path))
+        print('Download finish!')
+
+    detector = cv2.FaceDetectorYN.create(str(detection_model_path), "", (image_width, image_height))
+    recognizer = cv2.FaceRecognizerSF.create(str(recognition_model_path), "")
+
+    for image_path in tqdm(list(input_dir.glob('*.jpg'))):
+        image = cv2.imread(str(image_path))
+
+        ret, faces = detector.detect(image)
+        if faces is None:
+            continue
+        face_aligned = recognizer.alignCrop(image, faces[0])
+        cv2.imwrite(str(output_dir / f'{image_path.name}'), face_aligned, [cv2.IMWRITE_JPEG_QUALITY, 90])
